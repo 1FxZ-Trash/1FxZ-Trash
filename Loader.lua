2699fbde1d50bdc533c599d4333380abec13cb83
x2Switchz <102856517+1FxZ-Trash@users.noreply.github.com>
2022-10-11 10:51:11 +0700

    Update and rename Teleport.Lua to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,7 +7,7 @@ elseif PlaceId == 6381829480 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/1FxZ-Trash/1FxZ-Trash/main/KingLegacyy2"))()
 
 else
-	game.Players.LocalPlayer:kick("สคริปไม่มีเกมนี้ ไอ่สัส")
+	game.Players.LocalPlayer:kick("Script Don't Support this game")
 	wait(1)
 	game:Shutdown()
 end
